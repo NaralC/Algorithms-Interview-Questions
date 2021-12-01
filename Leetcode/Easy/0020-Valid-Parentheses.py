2aab2f92f50b5225b7c465a0e539003164f1b880
NaralC <77269201+NaralC@users.noreply.github.com>
2021-12-01 22:51:47 +0700

    Update

---
+++ Leetcode/Easy/0020-Valid-Parentheses.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        counterparts = {')': '(', '}': '{', ']': '['}
+        
+        for char in s:
+            #O(1) lookup time since there are only 3 parentheses
+            if char in counterparts.values():
+                stack.append(char)
+            else:
+                if stack == []: #In case the input is ")"
+                    return False
+                operand1 = stack.pop()
+                operand2 = counterparts[char]
+                
+                if operand1 != operand2: return False
+                
+        return True if stack == [] else False #In case the input is "("
