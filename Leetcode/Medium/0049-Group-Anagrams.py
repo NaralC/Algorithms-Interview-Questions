4120520937bbe485f0857385a4bba970f7833fae
NaralC <77269201+NaralC@users.noreply.github.com>
2021-12-16 22:32:49 +0700

    Update

--- Leetcode/Medium/0049-Group-Anagrams.py
+++ Leetcode/Medium/0049-Group-Anagrams.py
@@ -1,6 +1,21 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         return countCharAndHash(strs)
+    
+def sortAndHash(strs):
+    #Time: O(m * nlog(n)) where m = number of words, n = average word length
+    #Space: O(m * n)
+    output = {}
+    
+    for word in strs:
+        sortedWord = ''.join(sorted(word))
+        
+        if sortedWord in output:
+            output[sortedWord].append(word)
+        else:
+            output[sortedWord] = [word]
+        
+    return output.values()
 
 def countCharAndHash(strs):
     #Time: O(m * n) where m = number of words, n = average word length
@@ -8,30 +23,18 @@ def countCharAndHash(strs):
     output = {}
     
     for word in strs:
-        count = [0] * 26 #Counting the frequency of each alphabets
+        #Hash tables don't work since casting them to tuples in order to assign them as dict keys will group all values together in one array
+        
+        charFreq = [0] * 26 #where each idx represents 0-25 English alphabets
         
         for char in word:
-            #ord('a') = 97 -> ord('a') - 97 = 0
-            count[ord(char) - 97] += 1
+            #Eg. ord('a') - ord('a') = 0
+            charFreq[ord(char) - ord('a')] += 1
         
-        if tuple(count) in output:
-            output[tuple(count)].append(word)
+        if tuple(charFreq) in output:
+            output[tuple(charFreq)].append(word)
         else:
-            output[tuple(count)] = [word]
+            output[tuple(charFreq)] = [word]
+        
             
-    return output.values()
-    
-def sortAndHash(strs):
-    #Time: O(m * nlog(n)) where m = number of words, n = length of the longest word
-    #Space: O(m * n)
-    output = {}
-
-    for word in strs:
-        sortedWord = ''.join(sorted(word))
-
-        if sortedWord in output:
-            output[sortedWord].append(word)
-        else:
-            output[sortedWord] = [word]
-
     return output.values()
