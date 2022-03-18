cfa25d0bddf2cc17b046a53972de4b6274bb2518
NaralC <77269201+NaralC@users.noreply.github.com>
2022-03-18 23:54:06 +0700

    Update

--- Leetcode/Medium/0316-Remove-Duplicate-Letters.py
+++ Leetcode/Medium/0316-Remove-Duplicate-Letters.py
@@ -5,7 +5,7 @@ class Solution:
         #Using an array of size 26 to track and turn chars into a string doesn't work since the output has to be a subsequence of s
         #In terms of lexicography and greediness, keep smaller chars at the front while making sure every char has a place in the final output
         
-        lastIdx = {char : idx for idx, char in enumerate(s)} #Prevent appending the last one any a char's kind
+        lastIdx = {char : idx for idx, char in enumerate(s)} #Prevent appending the last one any char's kind
         seen = set() #Prevent duplicates in the stack
         stack = [] #Monotonic - small chars at the bottom
         
