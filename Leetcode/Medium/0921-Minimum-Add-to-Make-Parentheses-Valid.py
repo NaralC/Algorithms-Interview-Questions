b0ddfbccac6597e518c561f86e08c2a8850e3476
NaralC <77269201+NaralC@users.noreply.github.com>
2022-04-01 22:51:19 +0700

    Update

--- Leetcode/Medium/0921-Minimum-Add-to-Make-Parentheses-Valid.py
+++ Leetcode/Medium/0921-Minimum-Add-to-Make-Parentheses-Valid.py
@@ -1,22 +1,43 @@
 class Solution:
     def minAddToMakeValid(self, s: str) -> int:
-        # Time: O(n)
-        # Space: O(1)
+        return greedy(s)
         
-        unmatchedOpen = unmatchedClose = 0
+def utilizeStack(s):
+    # Time: O(n)
+    # Space: O(n)
+    
+    stack = []
+
+    for idx in reversed(range(len(s))):
+        char = s[idx]
+
+        stack.append(char)
+
+        if len(stack) >= 2 and stack[-1] == '(' and stack[-2] == ')':
+            stack.pop()
+            stack.pop()
+
+    return len(stack)
         
-        for char in s:
-            # Require a closing
-            if char == '(':
-                unmatchedOpen += 1
-            
-            # Require an opening
+def greedy(s):
+    # Time: O(n)
+    # Space: O(1)
+
+    unmatchedOpen = unmatchedClose = 0
+    output = 0
+
+    for char in s:
+        # Require a closing
+        if char == '(':
+            unmatchedOpen += 1
+
+        # Require an opening
+        else:
+            # There's an opening already available
+            if unmatchedOpen >= 1:
+                unmatchedOpen -= 1 # Use it up to make a valid pair
+            # There's no opening available
             else:
-                # There's an opening already available
-                if unmatchedOpen >= 1:
-                    unmatchedOpen -= 1 # Use it up to make a valid pair
-                # There's no opening available
-                else:
-                    unmatchedClose += 1
-        
-        return unmatchedOpen + unmatchedClose
+                unmatchedClose += 1
+
+    return unmatchedOpen + unmatchedClose
