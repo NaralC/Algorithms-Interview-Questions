5f9ae2a7960ce5017785ce78da3ca5407ba88239
NaralC <77269201+NaralC@users.noreply.github.com>
2022-04-05 23:42:00 +0700

    Update

--- Leetcode/Medium/0045-Jump-Game-II.py
+++ Leetcode/Medium/0045-Jump-Game-II.py
@@ -1,35 +1,19 @@
 class Solution:
     def jump(self, nums: List[int]) -> int:
-        #Time: O(m^n) where m = max(nums), n = len(nums)
-        #Space: O(m^n)
-        def recursion(pos): #TLE
-            if pos >= len(nums) - 1:
-                return 0
-            
-            count = float('inf')
-            for steps in range(1, nums[pos] + 1):
-                count = min(count, 1 + recursion(pos + steps))
-                
-            return count
+        # Time: O(n)
+        # Space: O(1)
         
-        #Time: O(n) where n = len(nums)
-        #Space: O(1)
-        def greedy():
-            left, right = 0, 0
-            count = 0
-            
-            #Terminate when we hit the last position
-            while right < len(nums) - 1:
-                furthest = 0
-                
-                for steps in range(left, right + 1):
-                    furthest = max(furthest, steps + nums[steps])
-                
-                left = right + 1
-                right = furthest
-                count += 1
-                
-            return count
+        output = furthest = 0
         
-        #Pick your functions here
-        return greedy()
+        while furthest < len(nums) - 1:
+            output += 1 # Increment every time we make a jump
+            
+            # Find the furthest position we can jump to
+            leap = 0
+            for start in range(furthest + 1):
+                leap = max(leap, start + nums[start])
+            
+            # Update the furthest position we can reach
+            furthest = max(furthest, leap)
+        
+        return output
