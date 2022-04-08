9747c42e785b4ae7f621998d82864f589ba64680
NaralC <77269201+NaralC@users.noreply.github.com>
2022-04-08 16:19:45 +0700

    Update

--- Leetcode/Medium/0130-Surrounded-Regions.py
+++ Leetcode/Medium/0130-Surrounded-Regions.py
@@ -1,40 +1,64 @@
+from collections import deque
+
 class Solution:
     def solve(self, board: List[List[str]]) -> None:
-        """
-        Do not return anything, modify board in-place instead.
-        """
-        #Time: O(m * n) iterating through every single element
-        #Space: O(m * n) worst case where the entire board is purely made of O's
+        # Time: O(mn)
+        # Space: O(mn)
+        # Any 'O' that is not on the border and it is not connected to an 'O' on the border will be flipped to 'X'
+
+#         def capture(row, col):
+#             # Prevent boundary break
+#             if row < 0 or col < 0 or row >= len(board) or col >= len(board[0]):
+#                 return
+            
+#             # Only proceed if the current cell is an O
+#             if board[row][col] != 'O':
+#                 return
+#             board[row][col] = 'C'
+            
+#             capture(row + 1, col)
+#             capture(row - 1, col)
+#             capture(row, col + 1)
+#             capture(row, col - 1)
         
-        #Traverse the border and capture any spots connected to border O's witn DFS
+        def capture(a, b):
+            q = deque()
+            q.append((a, b))
+            
+            while len(q):
+                # Only proceed if the current cell is an O
+                row, col = q.popleft()
+                
+                if board[row][col] != 'O':
+                    continue
+                board[row][col] = 'C'
+                
+                # Append its neighbors back to q
+                if row > 0: q.append((row - 1, col)) # Top
+                    
+                if row < len(board) - 1: q.append((row + 1, col)) # Right
+                    
+                if col > 0: q.append((row, col - 1)) # Left
+                    
+                if col < len(board[0]) - 1: q.append((row, col + 1)) # Bot
+                    
+        
+        # Traverse the border -> Capture non-border O's connected to border ones
         for row in range(len(board)):
             for col in range(len(board[0])):
-                if board[row][col] == 'O' and (row in [0, len(board) - 1] or col in [0, len(board[0]) - 1]):
-                    capture(row, col, board)
-                    
-        #Flip uncaptured spots, as they are not connected to any border O's
+                if row == 0 or col == 0 or row == len(board) - 1 or col == len(board[0]) - 1:
+                    if board[row][col] == 'O':
+                        capture(row, col)
+                        
+        # Traverse the entire board -> Flip all O's into X's
         for row in range(len(board)):
             for col in range(len(board[0])):
                 if board[row][col] == 'O':
                     board[row][col] = 'X'
         
-        #Turn back captured spots to the original state
+        # Traverse the entire board -> Revert captured ('C') cells back to O's
         for row in range(len(board)):
             for col in range(len(board[0])):
                 if board[row][col] == 'C':
                     board[row][col] = 'O'
-    
-def capture(row, col, board):
-    #Boundary check
-    if row < 0 or row > len(board) - 1 or col < 0 or col > len(board[0]) - 1:
-        return
-    #Keep the spreading process within O's and connected spots
-    if board[row][col] != 'O':
-        return
-    
-    board[row][col] = 'C' #C = Captured
-    capture(row - 1, col, board) #Capture neighboring spots
-    capture(row + 1, col, board)
-    capture(row, col - 1, board)
-    capture(row, col + 1, board)
         
--- Leetcode/Medium/0153-Find-Minimum-in-Rotated-Sorted-Array.py
+++ Leetcode/Medium/0153-Find-Minimum-in-Rotated-Sorted-Array.py
@@ -1,30 +1,24 @@
-from errno import EILSEQ
-
-
 class Solution:
     def findMin(self, nums: List[int]) -> int:
-        #Time: O(logn)
-        #Space: O(1)
+        # Time: O(logn)
+        # Space: O(1)
         
-        output = nums[0]
-        left, right = 0, len(nums) - 1
+        # Edge cases: if nums is already sorted, or has a length of 1
+        if nums[0] <= nums[-1]:
+            return nums[0]
         
-        while left <= right:
-            #Our boundary is exclusively on the smaller sorted side
-            if nums[left] < nums[right]:
-                return min(output, nums[left])
+        l, r = 0, len(nums) - 1
+        
+        while l <= r:
+            mid = (l + r) // 2
             
-            #Our boundary is not exclusively on the smaller sorted side yet
-            else:
-                mid = (left + right) // 2
-                output = min(output, nums[mid])
-                
-                #Mid still hangs on the bigger side
-                if nums[left] <= nums[mid]:
-                    left = mid + 1
-                
-                #Mid already on the smaller side
-                else:
-                    right = mid - 1
+            if nums[mid] > nums[mid + 1]:
+                return nums[mid + 1]
         
-        return output
+            # Mid currently on the left side -> Go left til we find the minimum
+            if nums[mid] >= nums[0]:
+                l = mid + 1
+            
+            # Mid currently on the right side -> Go right til we find the minimum
+            else:
+                r = mid - 1
