8e25b806b4d8e28901e376fb210d804cc2e6cb91
NaralC <77269201+NaralC@users.noreply.github.com>
2022-04-12 23:14:04 +0700

    Update

--- Leetcode/Medium/0128-Longest-Consecutive-Sequence.py
+++ Leetcode/Medium/0128-Longest-Consecutive-Sequence.py
@@ -1,22 +1,46 @@
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
-        #Time: O(n)
-        #Space: O(n)
+        return hashSet(nums)
         
-        nums.sort()
-        longest = 0
-        seen = set(nums)
+def hashSet(nums):
+    # Time: O(n)
+    # Space: O(n)
+
+    output = 0
+    lookup = set(nums)
+
+    # Check for the longest sequence starting from every number
+    for num in nums:
+        # A number qualifies as a start if a number smaller than it by 1 doesn't exist
+        if num - 1 not in lookup:
+            length = 1
+
+            while num + length in lookup:
+                length += 1
+
+            output = max(output, length)
+
+    return output
         
-        for num in nums:
-            #See it the current number could be a start to a sequence
-            if num - 1 not in seen:
-                length = 1
-                
-                #Keep going to see how long the sequence is
-                while num + length in seen:
-                    length += 1
-                
-                #Update our variable
-                longest = max(longest, length)
-        
-        return longest
+def sorting(nums):
+    # Time: O(nlogn)
+    # Space: O(1)
+    if not len(nums): return 0
+
+    output = 0
+    runningLength = 1
+    nums.sort()
+
+    for idx in range(len(nums) - 1):
+        left, right = nums[idx], nums[idx + 1]
+
+        if left == right:
+            continue
+
+        if left + 1 == right:
+            runningLength += 1
+        else:
+            output = max(output, runningLength)
+            runningLength = 1
+
+    return max(output, runningLength)
--- Leetcode/Medium/0238-Product-of-Array-Except-Self.py
+++ Leetcode/Medium/0238-Product-of-Array-Except-Self.py
@@ -1,44 +1,23 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        return memoize(nums)
-    
-def bruteforce(nums):
-    #Time: O(n^2)
-    #Space: O(1) excluding the output
-    output = []
-    
-    for idx in range(len(nums)):
-        newNums = nums[:idx] + nums[idx + 1:]
-        curProd = 1
+        # Time: O(n)
+        # Space: O(1)
         
-        for num in newNums:
-            curProd *= num
+        # Assign the current running product to the output off by 1 position depending on the side of approach
+        output = [1 for _ in range(len(nums))]
         
-        output.append(curProd)
-    
-    return output
+        # Compute running product from the left
+        prod = 1
         
-def memoize(nums):
-    #Time: O(n)
-    #Space: O(n) excluding the output
-    output = [1] * len(nums)
-    leftProds = [1] * len(nums)
-    rightProds = [1] * len(nums)
-
-    curProd = 1
-    for idx in range(len(nums)):
-        curProd *= nums[idx]
-        leftProds[idx] = curProd
-
-    curProd = 1
-    for idx in reversed(range(len(nums))):
-        curProd *= nums[idx]
-        rightProds[idx] = curProd
-
-    for idx in range(len(output)):
-        left = leftProds[idx - 1] if idx > 0 else 1
-        right = rightProds[idx + 1] if idx < len(nums) - 1 else 1
-
-        output[idx] = left * right
-
-    return output
+        for idx in range(len(nums) - 1):
+            prod *= nums[idx]
+            output[idx + 1] *= prod
+        
+        # Compute running product from the right
+        prod = 1
+        
+        for idx in range(len(nums) - 1, 0, -1):
+            prod *= nums[idx]
+            output[idx - 1] *= prod
+        
+        return output
