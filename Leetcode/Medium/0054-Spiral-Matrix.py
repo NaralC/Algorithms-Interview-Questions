d274ef0291a11b8f075b537209417e559560caae
NaralC <77269201+NaralC@users.noreply.github.com>
2022-04-15 23:41:11 +0700

    Update

--- Leetcode/Medium/0054-Spiral-Matrix.py
+++ Leetcode/Medium/0054-Spiral-Matrix.py
@@ -1,40 +1,42 @@
 class Solution:
     def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
-        #Time: O(height * width)
-        #Space: O(height * width)
+        # Time: O(mn)
+        # Space: O(mn)
         
-        height, width = len(matrix), len(matrix[0])
-        output = []
-        left, right = 0, width - 1
-        top, bot = 0, height - 1
+        nums = []
+        
+        left, right = 0, len(matrix[0]) - 1
+        top, bot = 0, len(matrix) - 1
         
         while left < right and top < bot:
-            #Top left -> top right
+            # Top left -> Top right
             for col in range(left, right):
-                output.append(matrix[top][col])
+                nums.append(matrix[top][col])
             
-            #Top right -> bot right
+            # Top right -> Bot right
             for row in range(top, bot):
-                output.append(matrix[row][right])
-                
-            #Bot right -> bot left
-            for col in range(right, left, -1):
-                output.append(matrix[bot][col])
-                
-            #Bot left -> top left
-            for row in range(bot, top, -1):
-                output.append(matrix[row][left])
+                nums.append(matrix[row][right])
             
-            left += 1
-            right -= 1
-            top += 1
-            bot -= 1
+            # Bot right -> Bot left
+            for col in range(right, left, -1):
+                nums.append(matrix[bot][col])
+            
+            # Bot left -> Top left
+            for row in range(bot, top, -1):
+                nums.append(matrix[row][left])
+            
+            # Move boundary inwards
+            left += 1; right -= 1
+            top += 1; bot -= 1
         
-        #Edge case: a leftover row or column in the middle
-        #Perform a scan, any order is fine because the leftover is either a row/column
-        if len(output) < width * height:
+        # Take care of a row/col of elements left in the middle
+        COLS, ROWS = len(matrix[0]), len(matrix)
+        
+        if len(nums) < COLS * ROWS:
+            # Sweep top -> bot
             for row in range(top, bot + 1):
+                # Sweep left -> right
                 for col in range(left, right + 1):
-                    output.append(matrix[row][col])
+                    nums.append(matrix[row][col])
         
-        return output
+        return nums
