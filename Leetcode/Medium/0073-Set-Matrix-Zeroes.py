5567f3de93ff06688e64f63d0b47d8e49c5f3383
NaralC <77269201+NaralC@users.noreply.github.com>
2022-04-23 23:00:36 +0700

    Update

--- Leetcode/Medium/0073-Set-Matrix-Zeroes.py
+++ Leetcode/Medium/0073-Set-Matrix-Zeroes.py
@@ -1,51 +1,36 @@
 class Solution:
     def setZeroes(self, matrix: List[List[int]]) -> None:
-        """
-        Do not return anything, modify matrix in-place instead.
-        """
-        constantSpace(matrix)
+        # Time: O(mn)
+        # Space: O(1)
         
-def constantSpace(matrix):
-    #Time: O(m * n) looping through every element
-    #Space: O(1)
-
-    firstRowZero, firstColZero = False, False
-    
-    for row in range(len(matrix)):
-        for col in range(len(matrix[row])):
-            if matrix[row][col] == 0:
-                if row == 0: firstRowZero = True
-                if col == 0: firstColZero = True
-                
+        ROWS, COLS = len(matrix), len(matrix[0])
+        
+        # Flag a cell by flipping its top most col and left most row to 0
+        # How do we flag the top most row and the left most col? Use extra variables
+        setZeroFirstRow = setZeroFirstCol = False
+        
+        for row in range(ROWS):
+            for col in range(COLS):
+                if not matrix[row][col]:
+                    if not row:
+                        setZeroFirstRow = True
+                    if not col:
+                        setZeroFirstCol = True
+                        
+                    matrix[row][0] = matrix[0][col] = 0
+        
+        
+        # Update cells according to their flags but skip the first row and col
+        for row in range(1, ROWS):
+            for col in range(1, COLS):
+                if not matrix[row][0] or not matrix[0][col]:
+                    matrix[row][col] = 0
+                    
+        # Clean up the top most row and the left most col
+        if setZeroFirstRow:
+            for col in range(COLS):
                 matrix[0][col] = 0
-                matrix[row][0] = 0
-                
-    for row in range(1, len(matrix)):
-        for col in range(1, len(matrix[row])):
-            if matrix[0][col] == 0 or matrix[row][0] == 0:
-                matrix[row][col] = 0
-                
-    if firstRowZero:
-        for col in range(len(matrix[0])):
-            matrix[0][col] = 0
-    
-    if firstColZero:
-        for row in range(len(matrix)):
-            matrix[row][0] = 0
         
-def extraSpace(matrix):
-    #Time: O(m * n) looping through every element
-    #Space: O(m + n) in the worst case where all rows and cols need to be set to zero
-
-    rows, cols = set(), set()
-
-    for row in range(len(matrix)):
-        for col in range(len(matrix[0])):
-            if matrix[row][col] == 0:
-                rows.add(row)
-                cols.add(col)
-
-    for row in range(len(matrix)):
-        for col in range(len(matrix[0])):
-            if row in rows or col in cols:
-                matrix[row][col] = 0
+        if setZeroFirstCol:
+            for row in range(ROWS):
+                matrix[row][0] = 0
