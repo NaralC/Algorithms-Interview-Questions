82390e9740752b6c78f68204649823db9d85d342
NaralC <77269201+NaralC@users.noreply.github.com>
2022-04-30 23:28:19 +0700

    Update

--- Leetcode/Medium/0062-Unique-Paths.py
+++ Leetcode/Medium/0062-Unique-Paths.py
@@ -1,25 +1,18 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        #Time: O(n * m)
-        #Space: O(n * m)
-        memoize = [[0 for _ in range(n)] for _ in range(m)]
+        # Time: O(mn)
+        # Space: O(mn)
+        
+        dp = [[0 for _ in range(n)] for _ in range(m)]
         
         for row in range(m):
             for col in range(n):
-                #Only 1 way to reach the top row: strictly going right
-                if row == 0:
-                    memoize[row][col] = 1
-                    
-                #Only 1 way to reach the left most column: strictly going down
-                elif col == 0:
-                    memoize[row][col] = 1
-                    
-                else:
-                #Ways to reach current spot = ways to reach its left + ways to reach its top
-                    waysToReachLeft = memoize[row][col - 1] 
-                    waysToReachTop = memoize[row - 1][col]
-                    memoize[row][col] = waysToReachLeft + waysToReachTop
-        
-        #Return the ways to reach the bottom right spot
-        return memoize[-1][-1]
+                # The top most row and leftmost col only have 1 way to reach since the robot can only go down or right
+                if row == 0 or col == 0:
+                    dp[row][col] = 1
                 
+                # Other cells can add up ways from its their left and their top
+                else:
+                    dp[row][col] = dp[row - 1][col] + dp[row][col - 1]
+        
+        return dp[-1][-1]
