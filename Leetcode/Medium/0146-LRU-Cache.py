d753e5b1a7e2e7cb05cbb962751116228a2aba66
Naral <77269201+NaralC@users.noreply.github.com>
2022-09-02 00:57:09 +0700

    Update 0146-LRU-Cache

--- Leetcode/Medium/0146-LRU-Cache.py
+++ Leetcode/Medium/0146-LRU-Cache.py
@@ -1,69 +1,56 @@
 class Node:
-    def __init__(self, key, val, left = None, right = None):
-        self.key = key
-        self.val = val
-        self.left = left
-        self.right = right
+    def __init__(self, key, val):
+        self.key, self.val = key, val
+        self.prev = self.next = None
 
 class LRUCache:
-
     def __init__(self, capacity: int):
         self.cap = capacity
-        self.cache = dict() # {key : reference to node}
+        self.lookup = dict() # { key : reference to node }
         
-        # Initialize front and back nodes, and make the point at each other
-        self.back = Node('back', 'back')
-        self.front = Node('front', 'front')
-        
-        self.back.right = self.front
-        self.front.left = self.back
+        # Old <-> New
+        self.old = Node(float('-inf'), float('-inf'))
+        self.new = Node(float('inf'), float('inf'))
+        self.old.next = self.new
+        self.new.prev = self.old
 
     def get(self, key: int) -> int:
-        if key not in self.cache:
+        if key not in self.lookup:
             return -1
         
-        # Update the node: remove it from the list, and put it at the front
-        node = self.cache[key]
-        self.removeFromList(node)
-        self.insertAtFront(node)
-        return node.val
+        # Remove from middle of list and insert at the front
+        self.remove(self.lookup[key])
+        self.insert(self.lookup[key])
+        return self.lookup[key].val
 
     def put(self, key: int, value: int) -> None:
-        # Insert a new node at the front - inbetween the most recently used one and the front node
-        if key not in self.cache:
-            newNode = Node(key, value)
-            self.insertAtFront(newNode)
-            self.cache[key] = newNode
+        # Remove from middle of list if the node exists
+        if key in self.lookup:
+            self.remove(self.lookup[key])
         
-        # Update the node if it's already here - remove it from the list, and put in at the front
-        else:
-            node = self.cache[key]
-            node.val = value
-            self.removeFromList(node)
-            self.insertAtFront(node)
+        # Update the key and value then insert the node to the front
+        self.lookup[key] = Node(key, value)
+        self.insert(self.lookup[key])
         
-        # Evict the least recent node if the cap is exceeded
-        if len(self.cache) > self.cap:
-            self.evict_lru_node()
-    
-    # Make the most recent node and the front one point at the new node
-    def insertAtFront(self, node):
-        mostRecentNode = self.front.left
-        mostRecentNode.right = node
-        self.front.left = node
-        node.left = mostRecentNode
-        node.right = self.front
-    
-    # Make the neighboring nodes point at each other
-    def removeFromList(self, node):
-        node.left.right = node.right
-        node.right.left = node.left
-    
-    def evict_lru_node(self):
-        if len(self.cache) > 0:
-            leastRecentNode = self.back.right
-            self.cache.pop(leastRecentNode.key)
-            self.removeFromList(leastRecentNode)
+        # Evict oldest node should the quota's exceeded
+        if len(self.lookup) > self.cap:
+            toBeRemoved = self.old.next
+            self.remove(toBeRemoved)
+            del self.lookup[toBeRemoved.key]
+            
+    def remove(self, node):
+        # Make neighboring nodes point at each other
+        prev, nxt = node.prev, node.next
+        prev.next = nxt
+        nxt.prev = prev
+        
+    def insert(self, node):
+        # Make the 'new' node and the currently newest one point at our node
+        prev, nxt = self.new.prev, self.new
+        prev.next = node
+        node.prev = prev
+        nxt.prev = node
+        node.next = nxt
 
 # Your LRUCache object will be instantiated and called as such:
 # obj = LRUCache(capacity)
