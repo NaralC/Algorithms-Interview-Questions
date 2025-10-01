a7e805dcb68cde895718553ea0893bd27c860125
Naral <77269201+NaralC@users.noreply.github.com>
2025-10-01 20:43:02 +0700

    Update 0424-Longest-Repeating-Character-Replacement.py

--- Leetcode/Medium/0424-Longest-Repeating-Character-Replacement.py
+++ Leetcode/Medium/0424-Longest-Repeating-Character-Replacement.py
@@ -1,25 +1,23 @@
-from collections import defaultdict
-
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
         # Time: O(n)
         # Space: O(n)
-        
-        lookup = defaultdict(int) # Tracks the frequency of each element present in current substring
-        longest = 0 # The output
-        
-        left = 0
-        for right in range(len(s)):
-            # Add a new char to current substring
-            lookup[s[right]] += 1
-            
-            # Shrink the substring/sliding window if other frequencies aside from the highest one exceed our replacement quota
+        output = 0
+        l, r = 0, 0
+        lookup = defaultdict(int)
+
+        while r < len(s):
+            # Added new char!
+            lookup[s[r]] += 1
+
+            # Shrink the substring if other frequencies aside from the highest one exceed our replacement quota
             # Check this by subtracting the highest frequency from the length of current substring
-            while (right - left + 1) - max(lookup.values()) > k:
-                lookup[s[left]] -= 1
-                left += 1
-            
-            # Update output
-            longest = max(longest, right - left + 1)
-        
-        return longest
+            while (r - l + 1 - max(lookup.values())) > k:
+                lookup[s[l]] -= 1
+                l += 1
+
+            # Update
+            output = max(output, r - l + 1)
+            r += 1
+
+        return output
