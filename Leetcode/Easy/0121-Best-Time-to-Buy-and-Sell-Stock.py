c058668be97886161393de7d86ef5410a1ccf008
Naral <77269201+NaralC@users.noreply.github.com>
2025-10-01 20:09:40 +0700

    Update 0121-Best-Time-to-Buy-and-Sell-Stock.py

--- Leetcode/Easy/0121-Best-Time-to-Buy-and-Sell-Stock.py
+++ Leetcode/Easy/0121-Best-Time-to-Buy-and-Sell-Stock.py
@@ -1,19 +1,16 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        #Time: O(n)
-        #Space: O(1)
-        
-        buy, sell = 0, 1
+        # Time: O(n)
+        # Space: O(1)
         output = 0
-        
+        buy, sell = 0, 1
+
         while sell < len(prices):
-            #1) Better profit
-            if (profit := prices[sell] - prices[buy]) > output:
-                output = profit
-            
-            #2) Lower buying price
-            if prices[buy] > prices[sell]:
-                buy = sell
+            # Found a lower point to buy, move buy
+            if prices[sell] < prices[buy]: buy = sell
+
+            # Always scan for better profit
+            output = max(output, prices[sell] - prices[buy]) 
             
             sell += 1
         
