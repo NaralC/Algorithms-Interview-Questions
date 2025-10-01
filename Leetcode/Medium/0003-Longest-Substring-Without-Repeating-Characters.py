4bbbc47b35568424f1899c31be5910a6122c7c81
Naral <77269201+NaralC@users.noreply.github.com>
2025-10-01 20:21:38 +0700

    Update 0003-Longest-Substring-Without-Repeating-Characters.py

--- Leetcode/Medium/0003-Longest-Substring-Without-Repeating-Characters.py
+++ Leetcode/Medium/0003-Longest-Substring-Without-Repeating-Characters.py
@@ -1,20 +1,22 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        maxLen = 0
+        # Time: O(n)
+        # Space: O(n)
         seen = set()
+        longest = 0
         l, r = 0, 0
 
         while r < len(s):
-            # New char, not a dupe
+            # New char, not a dupe. Extend window to right
             if s[r] not in seen:
                 seen.add(s[r])
-                maxLen = max(maxLen, len(seen))
+                longest = max(longest, len(seen)) # update length
                 r += 1
 
-            # New char, is a dupe
+            # New char, already existing. Shrink window from left
             elif s[r] in seen:
                 seen.remove(s[l])
                 l += 1
 
-        return maxLen
+        return longest
         
